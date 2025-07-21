ea645ccca5b969fe93dc99b4981396c042f2ff3d
Kendra Scheer <kendrasc@buffalo.edu>
2025-07-21 17:02:50 -0400

    Update Figure_3B_and_D_code.R

--- Figure_3/Figure_3B_and_D_code.R
+++ Figure_3/Figure_3B_and_D_code.R
@@ -5,26 +5,27 @@ library(ggpubr)
 
 setwd("~/Desktop/Amylase_Americas/")
 Genotype = read.delim("AllQuechua_Maya_PhasedChr1_no_missing_biallelic_no_EUR_maf_0.05_genotypes_recreate.txt", header = T)
-GeneToCN = read.delim("All_Amylase_I_think.txt", header = T)
+GeneToCN = read.delim("All_amylase_gene_copy_number.txt", header = T)
 
 Genotype = Genotype %>%
   dplyr::filter(POS > 103343000 & POS < 103960000 )
 
-Genotype <- subset(Genotype, select = -CHROM)
-Genotype <- Genotype %>%
+Genotype = subset(Genotype, select = -CHROM)
+Genotype = Genotype %>%
   mutate(POS = paste0("X", POS))
 rownames(Genotype) = Genotype$POS
-Genotype_transposed <- as.data.frame(t(Genotype[,-1])) 
-colnames(Genotype_transposed) <- Genotype$POS
-Genotype_transposed$ID <- rownames(Genotype_transposed) 
-rownames(Genotype_transposed) <- NULL
-Genotype_transposed <- Genotype_transposed %>%
+Genotype_transposed = as.data.frame(t(Genotype[,-1])) 
+colnames(Genotype_transposed) = Genotype$POS
+Genotype_transposed$ID = rownames(Genotype_transposed) 
+rownames(Genotype_transposed) = NULL
+Genotype_transposed = Genotype_transposed %>%
   select(ID, everything())
 
-GenetoCN_Pops <- GeneToCN %>%
+GenetoCN_Pops = GeneToCN %>%
   dplyr::filter(Pop %in% c("Lowland_Andeans", "Highland_Andeans", "Maya_Chiapas")) %>%
   dplyr::filter(Pop %in% c("Lowland_Andeans", "Highland_Andeans", "Maya_Chiapas")) %>%
   dplyr::filter(ID != "CNS0261883" & ID != "108337" & ID != "108333") %>%
+# Above samples are removed because they have 100% non-American ancestry in the amylase locus
   dplyr::mutate(Pop = dplyr::case_when(
     Pop %in% c("Lowland_Andeans", "Highland_Andeans") ~ "Quechua",
     TRUE ~ Pop
@@ -32,14 +33,14 @@ GenetoCN_Pops <- GeneToCN %>%
 ################################# SNP by Gene Copy Number ###############################
 GenetoCN_Pops_sorted = GenetoCN_Pops[order(GenetoCN_Pops$ID),]
 
-Genotype_transposed <- Genotype_transposed %>% 
+Genotype_transposed = Genotype_transposed %>% 
   mutate(ID_short = sub(".*_", "", ID)) %>% 
   select(ID_short, everything())
 
 Genotype_sorted = Genotype_transposed[order(Genotype_transposed$ID_short),]
-dataset_for_snp_analyses <- cbind(GenetoCN_Pops_sorted, Genotype_sorted[, -2])
+dataset_for_snp_analyses = cbind(GenetoCN_Pops_sorted, Genotype_sorted[, -2])
 
-dataset_for_snp_analyses[9:ncol(dataset_for_snp_analyses)] <- lapply(
+dataset_for_snp_analyses[9:ncol(dataset_for_snp_analyses)] = lapply(
   dataset_for_snp_analyses[9:ncol(dataset_for_snp_analyses)],
   function(x) factor(ifelse(x == "0|0", "Homozygous Ancestral",
                             ifelse(x == "1|1", "Homozygous Derived", "Heterozygous")),
@@ -47,9 +48,9 @@ dataset_for_snp_analyses[9:ncol(dataset_for_snp_analyses)] <- lapply(
 
 
 ###################### Top SNP by AMY1 Copy Number ##################
-my_comparisons <- list( c("Homozygous Ancestral", "Heterozygous"), c("Heterozygous", "Homozygous Derived"), c("Homozygous Ancestral", "Homozygous Derived") )
-my_comparisons_2 <- c("Quechua", "Maya_Chiapas")
+my_comparisons = list( c("Homozygous Ancestral", "Heterozygous"), c("Heterozygous", "Homozygous Derived"), c("Homozygous Ancestral", "Homozygous Derived") )
 
+  # Example SNV is the SNV from Figures 3B and 3C
 figure = dataset_for_snp_analyses %>%
   ggplot(., aes(x = X103614521, y = round(AMY1), color = Pop)) +
   geom_boxplot() +
@@ -65,7 +66,7 @@ figure = dataset_for_snp_analyses %>%
   theme( legend.position = "bottom")
 
 figure
-pdf(file = "test_SNP_figure.pdf", width = 5, height = 4)
+pdf(file = "SNP_figure.pdf", width = 5, height = 4)
 print(figure)
 dev.off()
 
