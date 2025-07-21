a6a26e3e5321189db58125252cd299a8226797cd
Kendra Scheer <kendrasc@buffalo.edu>
2025-07-21 16:32:43 -0400

    Update Figure_1E_code.R

--- Figure_1/Figure_1E_code.R
+++ Figure_1/Figure_1E_code.R
@@ -4,13 +4,12 @@ library(forcats)
 
 Admixture = read.csv("~/Desktop/Amylase_Americas/CSVs/Admixture.csv")
 
-Admixture_long <- Admixture %>%
+Admixture_long = Admixture %>%
   dplyr::filter(Pop != "QC_L") %>%
   pivot_longer(
     cols = starts_with("Fraction"),
     names_to = "Ancestry",
-    values_to = "Value"
-  )
+    values_to = "Value")
 
 Admixture_long$Pop <- fct_relevel(Admixture_long$Pop, "QC_H", "MAYA", "PEL", "MXL")
 
@@ -21,20 +20,17 @@ admix = ggplot(Admixture_long, aes(x = factor(ID), y = Value, fill = Ancestry)) 
     x = "Individuals",
     y = "Fraction",
     title = "Ancestry Proportions",
-    fill = "Ancestry"
-  ) +
+    fill = "Ancestry") +
   scale_fill_manual(
     name = "Ancestry",
     labels = c("African", "Amerindian", "East Asian", "European"),
     values = c("Fraction.AFR" = "#cd1076ff", "Fraction.AMR" = "#ffcc00ff", 
-               "Fraction.EAS" = "#ffaaeeff", "Fraction.EUR" = "#ff6600ff")
-  ) +
+               "Fraction.EAS" = "#ffaaeeff", "Fraction.EUR" = "#ff6600ff")) +
   theme_minimal() +
   theme(
-    axis.text.x = element_blank(), # Remove x-axis text labels
-    axis.ticks.x = element_blank(), # Remove x-axis ticks
-    panel.spacing = unit(0.5, "lines") # Adjust space between facets
-  )
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    panel.spacing = unit(0.5, "lines"))
 
 pdf("~/Desktop/Amylase_Americas/PDFs/test_admixture_pdf.pdf", width = 15, height = 3)
 print(admix)
