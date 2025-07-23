5ec40c40dc4bb272e44a89d994ceeda015d9dde1
Kendra Scheer <kendrasc@buffalo.edu>
2025-07-23 14:36:34 -0400

    Create Figure_S15.sh

---
+++ Supplementary/Figure_S15/Figure_S15.sh
@@ -0,0 +1,11 @@
+We used the same output files as Figure 4B and Figure S14 for Quechua.
+
+${PATH_TO_RELATE}/scripts/SampleBranchLengths/SampleBranchLengths.sh \
+        -i Quechua_PhasedChr1_no_missing_biallelic_no_EUR_${test} \
+        -o Quechua_PhasedChr1_no_missing_biallelic_no_EUR_bypop_sampled_${test}_EGLN1_rs1769792_coal \
+        -m 1.25e-8 \
+        --coal Quechua_PhasedChr1_no_missing_biallelic_no_EUR_bypop_${test}.coal \
+        --format b \
+        --num_samples 100 \
+        --first_bp 231462872 \
+        --last_bp 231462872
